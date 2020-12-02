e591c6552b65d60cef0cb72924055428635836d3
Lucas Kempe <lucas.kempe@icloud.com>
2020-12-02 17:45:14 -0300

    New files added 12/02/2020...

--- TP8/tempCodeRunnerFile.c
+++ TP8/tempCodeRunnerFile.c
@@ -1,40 +1,89 @@
+/*
+Ingresar un número entero positivo, luego mostrar un mensaje indicando si el mismo
+es o no perfecto.
+*/
+/* === Inclusiones de cabeceras ================================================================ */
+
 #include <stdio.h>
  
-int numControl();
-void es_perfecto(int num);
+/* === Declaracion de Funciones ================================================================ */
+
+int ingresarPositivo();
+int esPerfecto(int numero);
+
+/* === Funcion Principal ================================================================ */
+
+/*
+Ejecuta las funciones previamente declaras y si la respuesta es igual 1, muestra el mensaje
+de que el numero es perfecto, sino muestra el mensaje de que no lo es.
+*/
 int main() {
-    int num = numControl();
-    es_perfecto(num);
+    int numero = ingresarPositivo();
+    int respuesta = esPerfecto(numero);
+    if (respuesta == 1) {
+        printf("El numero es perfecto.\n");
+    } else {
+        printf("El numero no es perfecto.\n");
+    }
     return 0;
 }
 
-int numControl() {
-    int num;
+
+/* === Definicion de Funciones ================================================================ */
+
+
+/*
+Esta función pide al usuario que ingrese un numero y almacena el valor en la variable numero,
+si el valor del numero es menor o igual que 0, advierte al usuario que el numero debe
+ser mayor que 0, por la definición de numero perfecto y vuelve a pedirle el numero
+al final retorna el numero.
+*/
+int ingresarPositivo() {
+    int numero;
     do {
         printf("Ingrese un numero: ");
-        scanf("%d", &num);
-        if (num <= 0) {
+        scanf("%d", &numero);
+        if (numero <= 0) {
             printf("Porfavor, el numero debe ser un entero positivo...\n");
         }
-    } while (num <= 0);
-    return num;
+    } while (numero <= 0);
+    return numero;
 }
 
-void es_perfecto(int num) {
-    
-    int aux, sum = 0, i;
+/*
+Esta función controla si el numero es perfecto, es decir si es igual a la suma
+de sus divisores, para controlar esto se usa la variable "divisor", en un ciclo for
+el cual va iterando a traves de "divisor", se usa una variable auxiliar llamada "valor"
+para almacenar el valor del modulo de dividir el numero ingresado en el divisor, de esta forma
+se comprueban los divisores del numero, si es divisor, entonces se va sumando el divisor a la variable
+suma que inicialmente es 0, al final se comprueba si la variable suma es igual al numero, 
+es decir una vez mas, si la suma de los divisores del numero es igual al numero mismo,
+si esto es real, retorna una respuesta.
+*/
 
-    for (i = 1; i <= (num - 1); i++) {
+int esPerfecto(int numero) {
+
+    int valor, suma = 0, divisor, respuesta;
+
+    for (divisor = 1; divisor < numero; divisor++) {
         
-        aux = num % i;
-        if (aux == 0) {
-            sum = sum + i;
+        valor = numero % divisor;
+        if (valor == 0) {
+            suma += divisor;
         }
     }
 
-    if (sum == num) {
-        printf("El numero %i es perfecto.\n", num);
+    if (suma == numero) {
+        respuesta = 1;
     } else {
-        printf("El numero %i no es perfecto.\n", num);
+        respuesta = 0;
     }
+    return respuesta;
 }
+
+
+/*
+Creado por: Kempe Lucas Alejandro
+Ing Computación
+12/02/2020 
+*/
